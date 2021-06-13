6e4a765963ef395f509fc5732a231add932dd537
MaxThanachai <sobersobad@hotmail.com>
2021-06-14 01:06:01 +0700

    hw2

--- hw1/core/views.py
+++ hw1/core/views.py
@@ -1,6 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
-def index(request):
-    return render(request, 'index.html')
+from core.models import Profile
+from core.forms import SubscriberForm
+
+# def index(request):
+#     return render(request, 'index.html')
+
+
+class IndexView(View):
+    def get(self, request):
+        profile = Profile.objects.get(id=2)
+
+        form = SubscriberForm()
+
+        email = profile.email
+        phone_no = profile.phone_no
+        return render(
+            request,
+            'index.html', 
+            {
+                "email": email,
+                "phone_no": phone_no,
+                "form": form
+            }
+        )
+    
+    def post(self, request):
+        print(request.POST)
+        
+        profile = Profile.objects.get(id=2)
+        form = SubscriberForm(request.POST)
+        
+        if form.is_valid():
+            profile = Profile.objects.create(email=form.cleaned_data.get("email"))
+
+        email = profile.email
+        phone_no = profile.phone_no
+        return render(
+            request,
+            'index.html', 
+            {
+                "email": email,
+                "phone_no": phone_no,
+                "form": form
+            }
+        )
